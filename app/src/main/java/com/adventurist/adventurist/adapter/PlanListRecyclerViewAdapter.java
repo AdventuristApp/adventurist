b43b71fd8c0f0776e33931763256aa14759fddd7
Ghaidaa-Alrawad <ghaidaarowad@gmail.com>
2023-12-19 00:14:23 +0000

    alsoEditing

--- app/src/main/java/com/adventurist/adventurist/adapter/PlanListRecyclerViewAdapter.java
+++ app/src/main/java/com/adventurist/adventurist/adapter/PlanListRecyclerViewAdapter.java
@@ -6,15 +6,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.text.Html;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.ItemTouchHelper;
 
@@ -84,16 +89,53 @@ public class PlanListRecyclerViewAdapter extends RecyclerView.Adapter<PlanListRe
         });
     }
 
+//    private void showPopupMessage(Plan plan) {
+//        AlertDialog.Builder builder = new AlertDialog.Builder(callingActivity, R.style.CustomAlertDialog);
+//        builder.setTitle("Plan Details");
+//
+//        String message = "<br/><b style='color:#3366cc;'>Plan:</b> " + plan.getPlanName() +
+//                "<br/><b style='color:#3366cc;'>Days:</b> " + plan.getNumberOfDays() +
+//                "<br/><b style='color:#3366cc;'>Destination:</b> " + plan.getDestination() +
+//                "<br/><b style='color:#3366cc;'>Budget:</b> " + plan.getBudget();
+//
+//        TextView messageTextView = new TextView(callingActivity);
+//        messageTextView.setText(Html.fromHtml(message));
+//        messageTextView.setGravity(Gravity.CENTER);
+//
+//        builder.setView(messageTextView);
+//
+//        builder.setPositiveButton(Html.fromHtml("<font color='#870707'>Close</font>"), (dialog, which) -> {
+//        });
+//
+//        builder.create().show();
+//    }
+
+
     private void showPopupMessage(Plan plan) {
         AlertDialog.Builder builder = new AlertDialog.Builder(callingActivity, R.style.CustomAlertDialog);
-        builder.setTitle("Plan Details");
 
-        String message = "<b style='color:#3366cc;'>Plan:</b> " + plan.getPlanName() +
-                "<br/><b style='color:#3366cc;'>Days:</b> " + plan.getNumberOfDays() +
-                "<br/><b style='color:#3366cc;'>Destination:</b> " + plan.getDestination() +
-                "<br/><b style='color:#3366cc;'>Budget:</b> " + plan.getBudget();
+        TextView titleTextView = new TextView(callingActivity);
 
-        builder.setMessage(Html.fromHtml(message));
+        int paddingTop = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16, callingActivity.getResources().getDisplayMetrics());
+        titleTextView.setPadding(0, paddingTop, 0, 0);
+
+        titleTextView.setText("Plan Details");
+        titleTextView.setGravity(Gravity.CENTER);
+        titleTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+        titleTextView.setTextColor(ContextCompat.getColor(callingActivity, R.color.black));
+
+        builder.setCustomTitle(titleTextView);
+
+        String message = "<br/><br/><br/><b style='color:#3366cc;'>Plan:</b> " + plan.getPlanName() +
+                "<br/><br/><b style='color:#3366cc;'>Days:</b> " + plan.getNumberOfDays() +
+                "<br/><br/><b style='color:#3366cc;'>Destination:</b> " + plan.getDestination() +
+                "<br/><br/><b style='color:#3366cc;'>Budget:</b> " + plan.getBudget();
+
+        TextView messageTextView = new TextView(callingActivity);
+        messageTextView.setText(Html.fromHtml(message));
+        messageTextView.setGravity(Gravity.CENTER);
+
+        builder.setView(messageTextView);
 
         builder.setPositiveButton(Html.fromHtml("<font color='#870707'>Close</font>"), (dialog, which) -> {
         });
@@ -106,16 +148,17 @@ public class PlanListRecyclerViewAdapter extends RecyclerView.Adapter<PlanListRe
         builder.setTitle("Delete Plan");
         builder.setMessage("Are you sure you want to delete this plan?");
 
-        builder.setPositiveButton("Yes", (dialog, which) -> {
+        builder.setPositiveButton(Html.fromHtml("<font color='#870707'>Yes</font>"), (dialog, which) -> {
             deletePlan(plan, position);
             Snackbar.make(((AppCompatActivity) callingActivity).findViewById(android.R.id.content),
                     "Plan deleted", Snackbar.LENGTH_SHORT).show();
         });
 
-        builder.setNegativeButton("No", (dialog, which) -> dialog.dismiss());
+        builder.setNegativeButton(Html.fromHtml("<font color='#3366cc'>No</font>"), (dialog, which) -> dialog.dismiss());
 
         builder.create().show();
     }
+
 
     private void deletePlan(Plan plan, int position) {
         Amplify.API.mutate(
